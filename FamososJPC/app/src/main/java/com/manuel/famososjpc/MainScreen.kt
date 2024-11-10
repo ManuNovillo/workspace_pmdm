08b4384d5ca5a8a45b6931f3f18d0f6d6f6479d1
ManuNovillo <manuelnovilloinformatica@gmail.com>
2024-11-10 20:41:34 +0100

    app terminada

--- FamososJPC/app/src/main/java/com/manuel/famososjpc/MainScreen.kt
+++ FamososJPC/app/src/main/java/com/manuel/famososjpc/MainScreen.kt
@@ -2,6 +2,7 @@ package com.manuel.famososjpc
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -14,6 +15,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -22,7 +24,6 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -35,7 +36,7 @@ import androidx.compose.ui.unit.sp
 @Composable
 fun MainScreen(viewModel: MainViewModel) {
     Scaffold(
-        topBar = { CustomTopBar() },
+        topBar = { CustomTopBar(viewModel) },
         content = { padding ->
             CustomContent(padding, viewModel)
         }
@@ -45,9 +46,33 @@ fun MainScreen(viewModel: MainViewModel) {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CustomTopBar() {
-    TopAppBar(
-        title = { Text(text = "Famosos") },
+fun CustomTopBar(viewModel: MainViewModel) {
+    val racha by viewModel.racha
+    // CenterAlignedTopAppBar es como una TopAppBar pero centrada
+    // La TopAppBar normal está un poco desplazada a la derecha
+    CenterAlignedTopAppBar(
+        title = {
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp),
+            ) {
+                Text(
+                    text = "Famosos",
+                    modifier = Modifier
+                        .weight(1f),
+                    textAlign = TextAlign.Start,
+                )
+
+                Text(
+                    text = "Racha: $racha",
+                    modifier = Modifier
+                        .weight(1f),
+                    textAlign = TextAlign.End,
+                )
+            }
+        },
         colors = topAppBarColors(
             containerColor = MaterialTheme.colorScheme.primary,
             titleContentColor = MaterialTheme.colorScheme.background
@@ -73,7 +98,7 @@ fun CustomContent(padding: PaddingValues, viewModel: MainViewModel) {
 
 @Composable
 fun CardFamosos(i: Int, viewModel: MainViewModel) {
-    val personajesShow by viewModel.personajesShow.observeAsState()
+    val personajesShow = viewModel.personajesShow
     val coloresFotos = viewModel.coloresFotos
     val coloresNombres = viewModel.coloresNombres
     Card(
@@ -89,7 +114,7 @@ fun CardFamosos(i: Int, viewModel: MainViewModel) {
 
         Row(
             modifier = Modifier
-                .padding(horizontal = 10.dp ,vertical = 8.dp)
+                .padding(horizontal = 10.dp, vertical = 8.dp)
                 .background(Color.Transparent)
                 .fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween,
@@ -97,32 +122,29 @@ fun CardFamosos(i: Int, viewModel: MainViewModel) {
         ) {
             Image(
                 painter = painterResource(
-                    id = viewModel.mainActivity.resources.getIdentifier(
-                        "p${personajesShow!![i].id}", "drawable", viewModel.mainActivity.packageName
-                    )
+                    id = personajesShow[i].id.toInt()
                 ),
                 contentDescription = "Famoso",
                 modifier = Modifier
                     .weight(3f)
-                    .size(80.dp,120.dp)
+                    .size(80.dp, 120.dp)
                     .background(coloresFotos[i])
                     .padding(8.dp)
                     .clickable {
-                        if (coloresFotos[i] != Color.Green) viewModel.clickFoto(i)
+                        if (coloresFotos[i] != Color.Green) viewModel.onClickFoto(i)
                     }
             )
             Text(
-                text = personajesShow!![i].nombre,
+                text = personajesShow[i].nombre,
                 modifier = Modifier
 
                     .weight(9f)
                     .padding(16.dp)
                     .background(coloresNombres[i])
-                    .clickable { if (coloresNombres[i] != Color.Green) viewModel.clickName(i) },
+                    .clickable { if (coloresNombres[i] != Color.Green) viewModel.onClickName(i) },
                 textAlign = TextAlign.Center,
                 fontSize = 18.sp
             )
-
         }
     }
 }
--- FamososJPC/app/src/main/java/com/manuel/famososjpc/MainViewModel.kt
+++ FamososJPC/app/src/main/java/com/manuel/famososjpc/MainViewModel.kt
@@ -1,61 +1,159 @@
 package com.manuel.famososjpc
 
 import android.app.Activity.MODE_PRIVATE
-import android.util.Log
+import android.os.SystemClock
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.ui.graphics.Color
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.gson.Gson
 import com.manuel.famososjpc.entities.Person
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainViewModel(mainActivity: MainActivity) : ViewModel() {
     val mainActivity by lazy { mainActivity }
+
+    /**
+     * Color inicial de los elementos
+     */
+    private val colorInicial = Color.Transparent
+
+    /**
+     * Número de famosos a mostrar
+     */
     val numeroFamosos = 5
+
+    /**
+     * Número de aciertos seguidos
+     * No se resetea al cambiar de famosos, por lo que son posibles
+     * rachas mayores de 5
+     */
+    val racha = mutableIntStateOf(0)
+
+    /**
+     * Lista de todos los famosos
+     */
     private lateinit var personajes: ArrayList<Person>
+
+    /**
+     * Lista de los famosos que se mostrarán en el juego
+     */
     private var personajesGame = ArrayList<Person>()
-    private var _personajesShow = MutableLiveData(Array(5) { Person() })
-    val personajesShow: LiveData<Array<Person>> = _personajesShow
+
+    /**
+     * Lista de los objetos Person que se mostrarán en pantalla,
+     * los cuales NO se corresponden a famosos de verdad,
+     * sino que son instancias de utilidad para pasar a la MainScreen
+     * un nombre y una foto
+     */
+    val personajesShow = mutableStateListOf<Person>()
+
+    /**
+     * Lista de ids de los famosos que se muestran
+     */
     private val ids = ArrayList<String>()
+
+    /**
+     * Lista de nombres de los famosos que se muestran
+     */
     private val nombres = ArrayList<String>()
+
+    /**
+     * Lista con los índices de los nombres y fotos que han fallado
+     */
     private var errorAnterior = mutableListOf<Int>()
-    val coloresFotos =
-        mutableStateListOf<Color>()
-    val coloresNombres =
-        mutableStateListOf<Color>()
+
+    /**
+     * Número de aciertos en la ronda actual
+     */
+    private var aciertos = 0
+
+    /**
+     * Colores de fondo de las fotos
+     */
+    val coloresFotos = mutableStateListOf<Color>()
+
+    /**
+     * Colores de fondo de los nombres
+     */
+    val coloresNombres = mutableStateListOf<Color>()
+    // Inicializadas a -1 para indicar que no hay ninguna foto o nombre pulsado
+    // Un valor distinto significa que se ha pulsado una foto o un nombre, respectivamente
+
+    /**
+     * Índice de la foto pulsada
+     */
     private var pressedFoto = -1
+
+    /**
+     * Índice del nombre pulsado
+     */
     private var pressedName = -1
 
     init {
         getPersonajes()
-        startGame()
-        fillColors()
+        personajes.shuffle()
+        newRonda()
     }
 
-    private fun fillColors() {
+    /**
+     * Resetea los colores de las fotos y nombres,
+     * y les da el valor inicial
+     */
+    private fun resetColors() {
+        coloresFotos.clear()
+        coloresNombres.clear()
         for (i in 0 until numeroFamosos) {
-            coloresFotos.add(Color.Transparent)
-            coloresNombres.add(Color.Transparent)
+            coloresFotos.add(colorInicial)
+            coloresNombres.add(colorInicial)
         }
     }
 
-    private fun startGame() {
-        personajes.shuffle()
-        personajesGame = ArrayList(personajes.take(5))
-        _personajesShow.value = Array(5) { Person() }
+    /**
+     * Crea un nuevo intento de juego
+     */
+    private fun newRonda() {
+        resetColors()
+        // Limpiar todas las listas que contengan información sobre el intento
+        errorAnterior.clear()
+        ids.clear()
+        nombres.clear()
+        personajesShow.clear()
+        personajesGame = ArrayList()
+
+        // Añadir a la lista de personajesGame famosos aleatorios
+        for (i in 0 until numeroFamosos) {
+            personajesGame.add(personajes[(0..<personajes.size).random()])
+        }
+
         for (person in personajesGame) {
             nombres.add(person.nombre)
             ids.add(person.id)
         }
+
+        // Mezclar solo los nombres para dejar las fotos con el mismo orden
+        // que en la lista de personajesGame
         nombres.shuffle()
-//        ids.shuffle()
         for (i in 0 until numeroFamosos) {
-            _personajesShow.value!![i].nombre = nombres[i]
-            _personajesShow.value!![i].id = ids[i]
+            val id = mainActivity.resources.getIdentifier(
+                "p${ids[i]}", "drawable", mainActivity.packageName
+            )
+
+            /*
+             * Crear una nueva instancia de Person cuyo id es el id de la foto
+             * Esta instancia no corresponde a un famoso de verdad, es solo
+             * para facilitar el trabajo de mostar una foto y un nombre
+             * y para la comprobación de aciertos
+             */
+            personajesShow.add(Person(id.toString(), nombres[i]))
         }
     }
 
+    /**
+     * Obtiene los datos de los famosos del shared preferences
+     */
     private fun getPersonajes() {
         personajes = ArrayList()
         val personajesAll = mainActivity.getSharedPreferences("person", MODE_PRIVATE).all
@@ -66,25 +164,29 @@ class MainViewModel(mainActivity: MainActivity) : ViewModel() {
         }
     }
 
-    fun clickName(i: Int) {
+
+    fun onClickName(i: Int) {
         pressedName = i
+        // Si no hay foto pulsada, no comprobar
         if (pressedFoto == -1) return
         check(pressedFoto, pressedName)
     }
 
+    fun onClickFoto(i: Int) {
+        pressedFoto = i
+        // Si no hay nombre pulsado, no comprobar
+        if (pressedName == -1) return
+        check(pressedFoto, pressedName)
+    }
+
+    /**
+     * Comprueba si el nombre y la foto pulsados coinciden
+     * Si coinciden, a ambos se les pone el fondo verde.
+     * Si no, se pone rojo
+     * Si todos los nombres y fotos han sido ya unidos correctamente,
+     * se crea un nueva ronda
+     */
     private fun check(fotoPressed: Int, namePressed: Int) {
-//        var correcto = false
-//        val clickedPerson = Person(ids[fotoPressed], nombres[namePressed])
-//
-//        for (person in personajesGame) {
-//            Log.d("COJONES", "check: $person")
-//            Log.d("COJONES", "check: $clickedPerson")
-//            if (person == clickedPerson) {
-//                correcto = true
-//            }
-//        }
-//        if (correcto) coloresFotos[namePressed] = Color.Green
-//        else coloresFotos[namePressed] = Color.Red
         if (errorAnterior.isNotEmpty()) {
             coloresFotos[errorAnterior[0]] = Color.Transparent
             coloresNombres[errorAnterior[1]] = Color.Transparent
@@ -93,25 +195,31 @@ class MainViewModel(mainActivity: MainActivity) : ViewModel() {
             coloresFotos[fotoPressed] = Color.Green
             coloresNombres[namePressed] = Color.Green
             resetErrorAnterior()
+            racha.intValue++
+            aciertos++
+            if (aciertos == 5) {
+                // Esperar un poco antes de mostrar los nuevos famosos
+                GlobalScope.launch {
+                    SystemClock.sleep(500)
+                    launch(Main) {
+                        newRonda()
+                    }
+                }
+                aciertos = 0
+            }
         } else {
+            racha.intValue = 0
             coloresFotos[fotoPressed] = Color.Red
             coloresNombres[namePressed] = Color.Red
             resetErrorAnterior()
             errorAnterior.add(fotoPressed)
             errorAnterior.add(namePressed)
         }
-
         pressedFoto = -1
         pressedName = -1
     }
 
     private fun resetErrorAnterior() {
         errorAnterior.clear()
-    }
-
-    fun clickFoto(i: Int) {
-        pressedFoto = i
-        if (pressedName == -1) return
-        check(pressedFoto, pressedName)
     }
 }
