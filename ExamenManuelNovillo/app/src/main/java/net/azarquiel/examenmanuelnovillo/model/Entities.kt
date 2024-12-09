0e8793cf56b8781ec73c47a5c6add9f563859ae5
ManuNovillo <manuelnovilloinformatica@gmail.com>
2024-12-09 20:03:35 +0100

    plantilla con entities

--- ExamenManuelNovillo/app/src/main/java/net/azarquiel/examenmanuelnovillo/model/Entities.kt
+++ ExamenManuelNovillo/app/src/main/java/net/azarquiel/examenmanuelnovillo/model/Entities.kt
@@ -1,2 +1,32 @@
 package net.azarquiel.examenmanuelnovillo.model
 
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+import androidx.room.Relation
+
+//data class PuebloConProvincia(
+//    @Embedded val pueblo: Pueblo,
+//    @Relation(
+//        parentColumn = "provincia",
+//        entityColumn = "id"
+//    )
+//    val provincia: Provincia
+//)
+
+//@Entity(
+//    tableName = "provincia",
+//    foreignKeys = [
+//        ForeignKey(
+//            entity = Comunidad::class,
+//            parentColumns = ["id"],
+//            childColumns = ["comunidad"])
+//    ]
+//)
+//data class Provincia(
+//    @PrimaryKey
+//    var id: Int,
+//    var nombre: String = "",
+//    var comunidad: Int = 0
+//)
